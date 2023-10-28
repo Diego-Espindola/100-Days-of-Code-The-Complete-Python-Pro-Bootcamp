79ca6f3fda67fcee21b0cd788c5b43c14f7adcc6
Diego Espindola <diegoespindola9404@gmail.com>
2023-10-28 16:19:43 -0300

    Update day-1-printing-start.py.py

--- day 1 to day 15/day-1-printing-start.py.py
+++ day 1 to day 15/day-1-printing-start.py.py
@@ -1,11 +1,8 @@
-"""
-Instructions
-Write a program in main.py that prints the following notes on Python programming into the Output console. Your code should print all three lines.
-Day 1 - Python Print Function
-The function is declared like this:
-print('what to print')
-"""
+#Example of \n
+print("Hello world\nHello world\nHello world")
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+#Example of concatenation
+print("Hello" + " " + "Angela")
+
+#Example of \t
+print("Hello world\tHello world\tHello world")
