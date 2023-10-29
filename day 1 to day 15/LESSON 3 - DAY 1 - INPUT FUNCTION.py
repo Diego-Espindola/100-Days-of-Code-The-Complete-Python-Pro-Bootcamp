5d43957bda7ffbe90d5e0a3ce56ffd140cd302fc
Diego Espindola <diegoespindola9404@gmail.com>
2023-10-29 12:19:00 -0300

    Create LESSON 3 - DAY 1 - INPUT FUNCTION.py

---
+++ day 1 to day 15/LESSON 3 - DAY 1 - INPUT FUNCTION.py
@@ -0,0 +1,28 @@
+# Number one
+"""
+Try this exercise to understand how tests work with inputs in Auditorium. 
+Write some code that multiples together the two numbers in the input area. 
+When you run the tests, different inputs will replace the ones in the input area and pass the new inputs through your code.
+"""
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+
+# Number two
+"""
+Instructions
+Write a program that calculates and outputs the number of characters in any name.
+The automated tests will try out lots of different names as the input. 
+Your code should work for any name.
+"""
+
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+
+name = input()
+print(len(name))
+
