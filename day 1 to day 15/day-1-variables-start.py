34b3eec31e0c84afa6d975b45279f3f420ebbcd8
Diego Espindola <diegoespindola9404@gmail.com>
2023-10-29 12:37:47 -0300

    Update day-1-variables-start.py

--- day 1 to day 15/day-1-variables-start.py
+++ day 1 to day 15/day-1-variables-start.py
@@ -8,3 +8,15 @@ print(name)
 name = input("What is your name?")
 lenght = len(name)
 print(lenght)
+
+# Rules to follow:
+# Make your code readable
+# Try to make sure that it actually make sense to you
+# You can have multiple words in the name of your variable, example:
+
+user_name = input("What is your name?")
+# In python we use underscore in order to separate words.
+lenght1 = len(user_name)
+# You can't put numbers in the begining of the variable.
+print(user_name)
+# We cannot use the Python's built-in functions to name variables.
