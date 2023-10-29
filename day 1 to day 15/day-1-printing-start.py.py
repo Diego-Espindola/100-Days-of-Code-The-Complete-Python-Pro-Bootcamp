f163376224985cf91d5e1f4d756c77af199a5365
Diego Espindola <diegoespindola9404@gmail.com>
2023-10-28 21:13:52 -0300

    Update day-1-printing-start.py.py

--- day 1 to day 15/day-1-printing-start.py.py
+++ day 1 to day 15/day-1-printing-start.py.py
@@ -6,3 +6,9 @@ print("Hello" + " " + "Angela")
 
 #Example of \t
 print("Hello world\tHello world\tHello world")
+
+##### Input Function ######
+# input("A prompt for the user\n")
+
+# Will print the word "Hello" and the user input
+print("Hello " +input("What is your name?  ") )
