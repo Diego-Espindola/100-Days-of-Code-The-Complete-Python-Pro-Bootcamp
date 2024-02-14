c4b606e7d6738adc663ef33f42603671305fe343
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-13 21:54:22 -0300

    Create day-4-loops.py

---
+++ day 1 to day 15/Day 5/day-4-loops.py
@@ -0,0 +1,20 @@
+# # For loops with lists
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#   print(fruit)
+#   print(fruit + " Pie")
+# print(fruits)
+
+# # For loops with range
+# for number in range(0,10): # from 0 to 9, not including 10
+#   print(number)
+
+# for number in range(0,10,3): # from 0 to 9 with a jump (increment)
+#   print(number)
+
+
+# # Gauss sum
+# total = 0
+# for number in range(1,101):
+#   total += number
+# print(total)
