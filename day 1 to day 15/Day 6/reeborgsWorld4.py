bbf9b9862c48f18159e8f282db9a1423dd7c1ca3
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-14 19:23:15 -0300

    day 6

---
+++ day 1 to day 15/Day 6/reeborgsWorld4.py
@@ -0,0 +1,23 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+def turn_right():
+    for x in range(0,3):
+        turn_left()
+
+def jump():
+    turn_left()
+    move()
+    while not right_is_clear():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif wall_in_front():
+        jump()
+        
