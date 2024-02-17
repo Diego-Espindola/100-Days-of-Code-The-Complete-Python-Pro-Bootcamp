b47d3bfa812b324d069191fe2eca15969ba828d2
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-17 15:30:56 -0300

    Day 9

---
+++ day 1 to day 15/Day 9/Day-8-blind-auction-PROJECT/blind-auction-PROJECT.py
@@ -0,0 +1,34 @@
+from art import logo
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+
+def main():
+
+    print(logo)
+    print("Welcome to the secret auction program.")
+    other_bidders = True
+
+    bidders = {}
+
+    while other_bidders:
+        name = input("What is your name?: ")
+        bid = float(input("What's your bid?: $"))
+        bidders[name] = bid
+
+        have_next = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+        if have_next == "no":
+            other_bidders = False
+        clear()
+
+    # Checks the highest bid
+    highest_bid = 0
+    winner = ""
+    for key in bidders:
+        if bidders[key] > highest_bid:
+            highest_bid = bidders[key]
+            winner = key
+
+    print(f"The winner was {winner}. {bidders[winner]}")
+
+if __name__== "__main__":
+  main()
