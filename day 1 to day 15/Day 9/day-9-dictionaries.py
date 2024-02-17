5825305865e97b9823fd1928cd23f33be84f8563
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-17 15:37:20 -0300

    day 9

---
+++ day 1 to day 15/Day 9/day-9-dictionaries.py
@@ -0,0 +1,66 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+
+}
+# # Retrieving items from dictionary
+# print(programming_dictionary["Bug"])
+
+# # Adding new items to dictionary
+# programming_dictionary["Loop"] = "The action of doing something over and over again"
+# print(programming_dictionary)
+
+# # Create an empty dictionary
+# empty_dictionary = {}
+
+# # Wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# # Edit an item in a dictionary
+# programming_dictionary["Bug"] = "A moth in your computer"
+
+# # Loop through a dictionary
+# for key in programming_dictionary:
+#   value = programming_dictionary[key]
+#   print(key+" : "+ value)
+
+# # Nesting
+
+# capitals = {
+#   "France": "Paris",
+#   "Germany": "Berlin",  
+# }
+
+# # Nesting a List in a Dictionary
+
+# travel_log = {
+#   "France": ["Paris", "Lille", "Dijon"],
+#   "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+# }
+
+# # Nesting Dictionary in a Dictionary
+
+# travel_log = {
+#   "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#   "Brazil": {"cities_visited": ["Rio de Janeiro", "Florianópolis", "Salvador"], "total_visits}
+# }
+# print(travel_log['France']['cities_visited'])
+
+
+# # Nesting a Dictionary in a List
+# travel_log = [
+#   {
+#     "country": "France",
+#     "cities_visited": ["Paris", "Lille", "Dijon"],
+#     "total_visits": 12
+#   },
+#   {
+#     "country": "Germany",
+#     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#     "total_visits": 5
+#   }
+# ]
+
+# for travel in travel_log:
+#   print(travel['country'])
