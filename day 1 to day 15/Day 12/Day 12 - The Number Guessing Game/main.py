faffc603eca476230956bf0d7fa3c6d4f302dd91
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-20 22:32:04 -0300

    refactored the code

--- day 1 to day 15/Day 12/Day 12 - The Number Guessing Game/main.py
+++ day 1 to day 15/Day 12/Day 12 - The Number Guessing Game/main.py
@@ -1,15 +1,18 @@
 import random as r
 from art import logo
 
-NUMBER = r.randint(0,100)
-GUESSED = False
-def check_attempt(attempt):
-    global attempts
-    if attempt == NUMBER:
+def get_guess():
+    while True:
+        try:
+            return int(input("Make a guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+def check_attempt(number, guess):
+    if guess == number:
         return True
     else:
-        attempts -= 1
-        if attempt > NUMBER:
+        if guess > number:
             print("Too high")
         else:
             print("Too low")
@@ -17,24 +20,22 @@ def check_attempt(attempt):
 
 def main():
     print(logo)
-
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
+    
+    number = r.randint(0, 100)
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    if difficulty == "hard":
-        attempts = 5
-    else:
-        attempts = 10
+    attempts = 10 if difficulty == "easy" else 5
 
-    while attempts > 0 and not GUESSED:
+    while attempts > 0:
         print(f"You have {attempts} attempts remaining to guess the number.")
-        attempt = int(input("Make a guess: "))
-        GUESSED = check_attempt(attempt)
+        guess = get_guess()
+        if check_attempt(number, guess):
+            print(f"You got it! The answer was {number}.")
+            return
+        attempts -= 1
 
-    if(GUESSED):
-        print(f"You got it! The answer was {NUMBER}.")
-    else:
-        print(f"You've run out of guesses! The answer was {NUMBER}.")
+    print(f"You've run out of guesses! The answer was {number}.")
 
 if __name__ == "__main__":
     main()
