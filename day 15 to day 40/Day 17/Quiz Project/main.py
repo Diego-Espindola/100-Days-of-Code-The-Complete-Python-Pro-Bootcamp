945ad82f7fbd98d4f8baf5485583e7f524e03113
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-25 23:08:35 -0300

    day 17

--- day 15 to day 40/Day 17/Quiz Project/main.py
+++ day 15 to day 40/Day 17/Quiz Project/main.py
@@ -1,16 +1,22 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
+import random
 
 question_bank = [
     Question(question['text'], question['answer']) for question in question_data
 ]
 
+random.shuffle(question_bank)
+
 
 def main():
-    question_1 = Question("is 2 higher than 1?", "True")
-    if input(question_1.text) == question_1.answer:
-        print("Correct answer")
+    quiz_brain = QuizBrain(question_bank)
+    while quiz_brain.still_has_questions():
+        quiz_brain.next_question()
+
+    print("You've completed the quiz")
+    print(f"Your final score was {quiz_brain.score}/{quiz_brain.question_number}")
 
 
 if __name__ == "__main__":
