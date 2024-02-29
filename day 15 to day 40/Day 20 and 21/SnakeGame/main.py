416965cbc5a7ae9f3c572cd8d0de03014ffe6ef7
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-29 08:44:47 -0300

    day 20

--- day 15 to day 40/Day 20 and 21/SnakeGame/main.py
+++ day 15 to day 40/Day 20 and 21/SnakeGame/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+import random
 
 screen = Screen()
 screen.screensize(600, 600)
@@ -9,7 +10,13 @@ HEADING = 0
 
 FIRST_TURTLE = Turtle(shape="square")
 FIRST_TURTLE.color("White")
+FIRST_TURTLE.shapesize(stretch_wid=0.7, stretch_len=1, outline=5)
 FIRST_TURTLE.penup()
+
+
+def ate_fruit():
+    r = random.randint(0,1)
+    return r == 1
 
 
 def t_left():
@@ -51,16 +58,28 @@ def main():
     trace_heading()
     turtles = initiate()
     screen.update()
-
     is_game_on = True
     while is_game_on:
-        time.sleep(0.2)
+        time.sleep(1)
+        #TODO save the last heading of each turtle, but in
+        # first turtle we will have a problem about the person clicking and changing
+        # I think, if the person clicks then I shouldn't change the heading,
+        # but instead I should make a command to do something.
+        # so, when it's pressed a Key more than one time, it saves the command
         previous_position = FIRST_TURTLE.position()
         FIRST_TURTLE.forward(20)
         for other_turtle in turtles[1:]:
             aux_position = other_turtle.position()
+            # noinspection PyTypeChecker
             other_turtle.goto(previous_position)
+            check_heading(other_turtle)
             previous_position = aux_position
+        if ate_fruit():
+            new_turtle = FIRST_TURTLE.clone()
+            # noinspection PyTypeChecker
+            new_turtle.goto(previous_position)
+            turtles.append(new_turtle)
+
         screen.update()
 
 
