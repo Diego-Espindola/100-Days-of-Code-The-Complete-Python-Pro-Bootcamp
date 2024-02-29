7ce9ac18f0bea77994774e69db8036a0d08dfb9a
Diego Espindola <diegoespindola9404@gmail.com>
2024-02-28 23:21:08 -0300

    day 20

---
+++ day 15 to day 40/Day 20 and 21/SnakeGame/main.py
@@ -0,0 +1,67 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.screensize(600, 600)
+screen.tracer(0)
+screen.bgcolor("Black")
+HEADING = 0
+
+FIRST_TURTLE = Turtle(shape="square")
+FIRST_TURTLE.color("White")
+FIRST_TURTLE.penup()
+
+
+def t_left():
+    FIRST_TURTLE.setheading(90)
+
+
+def t_right():
+    FIRST_TURTLE.setheading(270)
+
+
+def t_forward():
+    FIRST_TURTLE.setheading(0)
+
+
+def t_backward():
+    FIRST_TURTLE.setheading(180)
+
+
+def trace_heading():
+    screen.listen()
+    screen.onkeypress(t_left, "Up")
+    screen.onkeypress(t_right, "Down")
+    screen.onkeypress(t_forward, "Right")
+    screen.onkeypress(t_backward, "Left")
+
+
+def main():
+    time.sleep(5)
+    trace_heading()
+
+    x = 0
+    turtles = [FIRST_TURTLE]
+    for _ in range(2):
+        x -= 20
+        turtle = FIRST_TURTLE.clone()
+        turtle.goto(x, 0)
+        turtles.append(turtle)
+
+    screen.update()
+
+    is_game_on = True
+    while is_game_on:
+        time.sleep(0.2)
+        previous_position = FIRST_TURTLE.position()
+        FIRST_TURTLE.forward(20)
+        for other_turtle in turtles[1:]:
+            aux_position = other_turtle.position()
+            other_turtle.goto(previous_position)
+            previous_position = aux_position
+        screen.update()
+
+
+if __name__ == "__main__":
+    main()
+    screen.exitonclick()
