294dc3812c6dc9c2a0909fdd28df9c355a02c8ae
Diego Espindola <diegoespindola9404@gmail.com>
2024-03-11 21:16:30 -0300

    Day 30

--- day 15 to day 40/Day 30/NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
+++ day 15 to day 40/Day 30/NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
@@ -4,12 +4,17 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Catch the KeyError when a user en
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
+while True:
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+        break
+    except KeyError:
+        print("Wrong entry, try again")
 print(output_list)
