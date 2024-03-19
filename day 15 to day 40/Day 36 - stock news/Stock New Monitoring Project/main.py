d4a02369f3c8d9d81ae0646ca7f97373ae1e461a
Diego Espindola <diegoespindola9404@gmail.com>
2024-03-18 22:42:33 -0300

    day 36

--- day 15 to day 40/Day 36 - stock news/Stock New Monitoring Project/main.py
+++ day 15 to day 40/Day 36 - stock news/Stock New Monitoring Project/main.py
@@ -4,6 +4,7 @@ from info import stock_api_key, news_api_key, phone_number, account_sid, auth_to
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+PERCENTAGE_LIMIT = 5
 
 
 def send_sms(body, to=phone_number):
@@ -41,35 +42,35 @@ def main():
     initial_value = float(data[the_day_before_yesterday]['1. open'])
     percentage_increase = ((final_value - initial_value) / initial_value) * 100
     print(percentage_increase)
-    if abs(percentage_increase) > 5:
-        print('Get News')
+    if abs(percentage_increase) > PERCENTAGE_LIMIT:
+         # print('Get News')
 
-    ## STEP 2: Use https://newsapi.org
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-    data = api_get(
-        url="https://newsapi.org/v2/everything",
-        q=COMPANY_NAME,
-        from_='2024-02-19',
-        sortBy="publishedAt",
-        apiKey=news_api_key
-    )
-    articles = data['articles'][:3]
-    print(articles)
-    ## STEP 3: Use https://www.twilio.com
-    # Send a separate message with the percentage change and each article's title and description to your phone number.
-    for article in articles:
-        headline = article['title']
-        brief = article['description']
-    # Optional: Format the SMS message like this:
-    """
-    TSLA: 🔺2%
-    Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-    Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-    or
-    "TSLA: 🔻5%
-    Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-    Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-    """
+        ## STEP 2: Use https://newsapi.org
+        # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+        data = api_get(
+            url="https://newsapi.org/v2/everything",
+            q=COMPANY_NAME,
+            from_='2024-02-19',
+            sortBy="publishedAt",
+            apiKey=news_api_key
+        )
+        articles = data['articles'][:3]
+        print(articles)
+        ## STEP 3: Use https://www.twilio.com
+        # Send a separate message with the percentage change and each article's title and description to your phone number.
+        for article in articles:
+            headline = article['title']
+            brief = article['description']
+        # Optional: Format the SMS message like this:
+        """
+        TSLA: 🔺2%
+        Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+        Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+        or
+        "TSLA: 🔻5%
+        Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+        Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+        """
 
 
 if __name__ == "__main__":
