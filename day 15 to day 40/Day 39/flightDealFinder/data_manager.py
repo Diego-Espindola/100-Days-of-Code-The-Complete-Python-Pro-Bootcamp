554ddbefdb5f248c0aa20193abb37eaa03df8757
Diego Espindola <diegoespindola9404@gmail.com>
2025-01-24 22:11:36 -0300

    Update data_manager.py

--- day 15 to day 40/Day 39/flightDealFinder/data_manager.py
+++ day 15 to day 40/Day 39/flightDealFinder/data_manager.py
@@ -1,10 +1,10 @@
 import requests
-
+from info import sheety_id
 
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        url_get: str = ""
+        url_get: str = f"https://api.sheety.co/${sheety_id}/flightDealsProject/prices"
 
     def get_data_sheet(self):
         pass
