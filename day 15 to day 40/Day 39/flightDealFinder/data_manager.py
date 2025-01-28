64516c8137ef3631cf451e77547e0d982b7cbdc3
Diego Espindola <diegoespindola9404@gmail.com>
2025-01-27 21:52:29 -0300

    continuing the project
    Class DataManager and enviroment variables created

--- day 15 to day 40/Day 39/flightDealFinder/data_manager.py
+++ day 15 to day 40/Day 39/flightDealFinder/data_manager.py
@@ -1,10 +1,21 @@
 import requests
-from info import sheety_id
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SHEETY_ID = os.getenv('sheety_id')
 
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        url_get: str = f"https://api.sheety.co/${sheety_id}/flightDealsProject/prices"
+        self.url_get: str = f"https://api.sheety.co/{SHEETY_ID}/flightDealsProject/prices"
+        self.destination_data = {}
 
-    def get_data_sheet(self):
-        pass
+    def get_destination_data(self):
+        try:
+            response = requests.get(self.url_get)
+            data = response.json()
+            self.destination_data = data['prices']
+        except Exception as e:
+            print('Erro ao buscar dados na planilha ', e)
+            return self.destination_data
--- day 15 to day 40/Day 39/flightDealFinder/main.py
+++ day 15 to day 40/Day 39/flightDealFinder/main.py
@@ -1,1 +1,13 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#This file will need to use the DataManager, FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from pprint import pprint
+from data_manager import DataManager as SheetyApiDataManager
+
+
+def main():
+    sheety_api_data_manager = SheetyApiDataManager()
+    sheet_data = sheety_api_data_manager.get_destination_data()
+    print(sheet_data)
+
+
+if __name__ == "__main__":
+    main()
